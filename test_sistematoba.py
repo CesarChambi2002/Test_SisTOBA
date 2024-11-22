51fec161b507195114fbb03473f2a111822e0275
CesarChambi2002 <ccj2022877@est.univalle.edu>
2024-11-22 18:01:17 -0400

    Update test_sistematoba.py

---
+++ test_sistematoba.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+#class TestToba:
+
+    #def setup_method(self):
