cac1f8a7e4c2b972addc575ecd6904abb05d7f41
CesarChambi2002 <ccj2022877@est.univalle.edu>
2024-11-29 18:03:52 -0400

    test
    test del login

--- test_sistematoba.py
+++ test_sistematoba.py
@@ -3,6 +3,34 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-#class TestToba:
+#driver = webdriver.Chrome()
 
-    #def setup_method(self):
+#driver.maximize_window()
+
+#driver.get('http://localhost/Ventas/login/')
+#time.sleep(2)
+#driver.find_element(By.XPATH, "//input[@name='email']").send_keys("grover@gmail.com")
+#time.sleep(2)
+#driver.find_element(By.XPATH, "//input[@name='password_user']").send_keys("12345678")
+   
+class TestInterfaz:
+    def setup_method(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.get('http://localhost/Ventas/login/')
+    
+    def teardown_method(self):
+        self.driver.quit()
+        print("Prueba Visual completada")
+    
+    time.sleep(2)
+    def test_verify_contenido_inicio(self):
+        actual = self.driver.find_element(By.XPATH, "//div[contains(@class,'card-header text-center')]//b").text 
+        esperada = "Sistema de Ventas"  
+        assert esperada in actual, f"Error. actual {actual}, esperado: {esperada}"
+        time.sleep(2)
+    
+    def test_insertar_usuario_contrasena(self):
+        self.driver.find_element(By.XPATH, "//input[@name='email']").send_keys("grover@gmail.com")
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//input[@name='password_user']").send_keys("12345678")
