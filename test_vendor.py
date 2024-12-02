8b2ed092a71146c51d8adee99e7c2df2673ee7df
Conradov1534 <vvc2022789@est.univalle.edu>
2024-12-02 11:15:48 -0400

    testeo correcto login
    funciona el testeo pero no ingresa el sistema

--- test_vendor.py
+++ test_vendor.py
@@ -2,31 +2,42 @@ from appium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from appium.options.android import UiAutomator2Options
+
+options = UiAutomator2Options()
+options.platform_name = "Android"
+options.platform_version = "14"
+options.device_name = "R5CT303H0VA"
+options.automation_name = "UiAutomator2"
+options.app = r"C:\Users\conra\AdministradorTOBA\app\build\outputs\apk\debug\app-debug.apk"
+options.app_package = "com.example.administradortoba"
+options.app_activity = "com.example.administradortoba.LoginActivity"
+options.uiautomator2_server_install_timeout = 60000  
 
 
-desired_caps = {
-    "platformName": "Android",                # Cambia a "iOS" si pruebas en iPhone
-    "platformVersion": "10",                 # Versión de Android o iOS
-    "deviceName": "emulator-5554",           # Nombre del dispositivo
-    "automationName": "UiAutomator2",        # Motor de automatización
-    "app": "/ruta/a/tu_aplicacion.apk",      # Ruta del archivo APK
-    "appPackage": "com.example.tuapp",       # Paquete de la app
-    "appActivity": "com.example.MainActivity" # Actividad principal
-}
-
-#sdsadsadsadsa
-driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
+driver = webdriver.Remote(
+    command_executor="http://127.0.0.1:4723", 
+    options=options
+)
 
 try:
-    
+  
     wait = WebDriverWait(driver, 10)
-    boton_iniciar_sesion = wait.until(EC.presence_of_element_located((By.ID, "btnLogin")))
+    
+    
+    campo_email = wait.until(EC.presence_of_element_located((By.ID, "edtUsuario")))
+    campo_email.send_keys("grover@gmail.com")  
 
     
+    campo_password = wait.until(EC.presence_of_element_located((By.ID, "edtPassword")))
+    campo_password.send_keys("12345678")  
+
+  
+    boton_iniciar_sesion = wait.until(EC.presence_of_element_located((By.ID, "btnLogin")))
     boton_iniciar_sesion.click()
 
-    print("Se hizo clic en el botón INICIAR SESIÓN correctamente.")
+    print("Correcto")
 
 finally:
- 
+  
     driver.quit()
