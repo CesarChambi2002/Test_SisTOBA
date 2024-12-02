5ad37fe5bedc01e13c3800e43aaeec1de449b2bb
Conradov1534 <vvc2022789@est.univalle.edu>
2024-12-02 18:02:55 -0400

    categoria terminado
    si se hace el testeo correcto pero no detecta el texto registrado

--- TEST_MOVIL/test_registrar_categoria.py
+++ TEST_MOVIL/test_registrar_categoria.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from appium.options.android import UiAutomator2Options
 
 class TestRegistrarCategoria:
-    def setup_method(self):
+    def setup(self):
         options = UiAutomator2Options()
         options.platform_name = "Android"
         options.platform_version = "14"
@@ -20,7 +20,7 @@ class TestRegistrarCategoria:
         self.driver = webdriver.Remote(command_executor="http://127.0.0.1:4723", options=options)
         self.wait = WebDriverWait(self.driver, 20)
     
-    def teardown_method(self):
+    def salir(self):
         self.driver.quit()
 
     def test_registrar_categoria(self):
@@ -30,7 +30,7 @@ class TestRegistrarCategoria:
         self.wait.until(EC.presence_of_element_located((By.ID, "btnLogin"))).click()
         self.wait.until(EC.presence_of_element_located((By.ID, "btnIrCategorias"))).click()
 
-        self.wait.until(EC.presence_of_element_located((By.ID, "edtNombreCategoria"))).send_keys("Nueva Categoría de Prueba5")
+        self.wait.until(EC.presence_of_element_located((By.ID, "edtNombreCategoria"))).send_keys("Nueva categoria")
 
         self.wait.until(EC.presence_of_element_located((By.ID, "btnInsertarCategoria"))).click()
      
@@ -39,12 +39,12 @@ class TestRegistrarCategoria:
         categorias = self.driver.find_elements(By.XPATH, "//android.widget.ListView/android.widget.TextView")
 
         if not categorias:
-            print("No se encontraron elementos en el ListView.")
+            print("no hay categorias con ese nombre")
         else:
-            print("Elementos encontrados en el ListView:")
+            print("elementos encontrados")
             for categoria in categorias:
                 print(f"- {categoria.text}")
 
         
         textos_categorias = [categoria.text for categoria in categorias]
-        assert "Nueva Categoría de Prueba" in textos_categorias, "La categoría no se agregó correctamente."
+        assert "Nueva prueba categoria" in textos_categorias, "la categoria no se agrego"
